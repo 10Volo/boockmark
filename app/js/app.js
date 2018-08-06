6a810c79d60a13c1ad1fa77b41b2873ae1c0ee38
10Volo <volodymyr.fenyak@gmail.com>
2018-08-06 20:27:47 +0300

    add CRUD and input filter

--- app/js/app.js
+++ app/js/app.js
@@ -7,17 +7,17 @@ var myApp = angular.module('webApp', ['ui.router'])
 			url: '/client',
 			templateUrl: 'client.html'
 		}
-		
+
 		var administratorState = {
 			name: 'administrator',
 			url: '/administrator',
 			templateUrl: 'administrator.html'
 		}
-		
+
 		$stateProvider.state(clientState);
 		$stateProvider.state(administratorState);
 	})
-	
+
 	// JSON bookmark
 	.controller('MainCtrl', function ($scope) {
 		$scope.bookmarks = [
@@ -31,42 +31,99 @@ var myApp = angular.module('webApp', ['ui.router'])
 			{"id": 7, "title": "github", "url": "http://github.com"},
 			{"id": 8, "title": "some", "url": "http://some.com"}
 		];
-	
+
 		// CRUD bookmark
 		$scope.isCreating = false;
 		$scope.isEditing = false;
-		
+
 		function starCreating() {
 			$scope.isCreating = true;
 			$scope.isEditing = false;
+
+			resetCreateForm();
+
 		}
-		
+
 		function cancelCreating() {
 			$scope.isCreating =false;
 		}
-		
+
 		function startEditing() {
 			$scope.isCreating = false;
 			$scope.isEditing = true;
 		}
-		
+
 		function cancelEditing() {
 			$scope.isEditing = false;
 		}
-		
-		function shouldShowCreating() {
-			return !$scope.isEditing;
-		}
-	
-		function shouldShowEditing() {
-			return $scope.isEditing && !$scope.isCreating;
-		}
-		
+
 		$scope.starCreating = starCreating;
 		$scope.cancelCreating = cancelCreating;
 		$scope.startEditing = startEditing;
 		$scope.cancelEditing = cancelEditing;
+
+
+		function shouldShowCreating() {
+			return !$scope.isEditing;
+		}
+
+		function shouldShowEditing() {
+			return $scope.isEditing && !$scope.isCreating;
+		}
+
 		$scope.shouldShowCreating = shouldShowCreating;
 		$scope.shouldShowEditing = shouldShowEditing;
-	});
+
+
+		function resetCreateForm() {
+			$scope.newBookmark = {
+				title: '',
+				url: ''
+			}
+		}
+
+		function createBookmark(bookmark) {
+			bookmark.id = $scope.bookmarks.length;
+			$scope.bookmarks.push(bookmark);
+
+			resetCreateForm();
+		}
+
+		$scope.createBookmark = createBookmark;
+
+		$scope.editedBookmark = null;
+
+		function setEditedBookmark(bookmark) {
+			$scope.editedBookmark = angular.copy(bookmark);
+		}
+		
+		function updateBookmark(bookmark) {
+			var index = _.findIndex($scope.bookmarks, function (b) {
+				return b.id == bookmark.id
+			});
+			$scope.bookmarks[index] = bookmark;
+			
+			$scope.editedBookmark = null;
+			$scope.isEditing = false;
+		}
+		
+		function isSelectedBookmark(bookmarkId) {
+			return $scope.editedBookmark !== null && $scope.editedBookmark.id === bookmarkId;
+		}
+
+		$scope.setEditedBookmark = setEditedBookmark;
+		$scope.updateBookmark = updateBookmark;
+		$scope.isSelectedBookmark = isSelectedBookmark;
+		
+		function deleteBookmark(bookmark) {
+			_.remove($scope.bookmarks, function(b) {
+				return b.id == bookmark.id;
+			})
+		}
+		
+		$scope.deleteBookmark = deleteBookmark;
+		
+	})
 ;
+
+
